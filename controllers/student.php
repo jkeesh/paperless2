4c9e3764c8310e568213288a2429a9a62e660159
Jeremy Keeshin <jkeesh@gmail.com>
2012-01-05 18:28:14 -0600

    basic student page

--- controllers/student.php
+++ controllers/student.php
@@ -1,7 +1,23 @@
 <?php
-	class StudentHandler extends ToroHandler {	
-		public function get() {
+
+	class StudentHandler extends ToroHandler {
 			
+		public function get($qid, $class, $student) {
+			$class_dir = Utilities::get_class_base($qid, $class);
+			$assns = Utilities::get_all_directories($class_dir . "/repos");
+			
+			$student_submissions = array();
+			foreach($assns as $assn){
+				$test = $class_dir . "/repos/" . $assn . "/" . $student;
+				if(is_dir($test)){
+					$student_submissions []= $assn;
+				}
+			}
+			
+
+			$this->smarty->assign("student", $student);
+			$this->smarty->assign("submissions", $student_submissions);
+			$this->smarty->display("student.html");			
 		}		
 	}
 ?>
