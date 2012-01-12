b0569a1fc084a307447f760996e601441df8ab25
Jeremy Keeshin <jkeesh@gmail.com>
2012-01-12 15:42:10 -0800

    add link to 149

--- controllers/default.php
+++ controllers/default.php
@@ -1,7 +1,8 @@
 <?php
 	class DefaultHandler extends ToroHandler {	
 		public function get() {
-			$links = array('CS107 Winter 2012' => ROOT_URL. '/1122/cs107/');
+			$links = array('CS107 Fall 2011' => ROOT_URL. '/1122/cs107/',
+						   'CS149 Winter 2012' => ROOT_URL. '/1124/cs149/');
 
 			$this->smarty->assign("links", $links);
 			$this->smarty->display("default.html");			
