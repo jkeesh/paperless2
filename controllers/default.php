ba124bbe1e056b87b44a613f548717a77dff6761
Jeremy Keeshin <jkeesh@gmail.com>
2012-04-03 20:24:59 -0700

    update listings

--- controllers/default.php
+++ controllers/default.php
@@ -1,10 +1,11 @@
 <?php
 	class DefaultHandler extends ToroHandler {	
 		public function get() {
-			$links = array('CS107 Fall 2011' => ROOT_URL. '/1122/cs107/',
+			$links = array('CS107 Spring 2012' => ROOT_URL. '/1126/cs107/',
+						   'CME213 Spring 2012' => ROOT_URL. '/1126/cme213/',
 						   'CS149 Winter 2012' => ROOT_URL. '/1124/cs149/',
 						   'CS107 Winter 2012' => ROOT_URL. '/1124/cs107/',
-						    'CME213 Spring 2012' => ROOT_URL. '/1126/cme213/');
+						   'CS107 Fall 2011' => ROOT_URL. '/1122/cs107/' );
 
 			$this->smarty->assign("links", $links);
 			$this->smarty->display("default.html");			
